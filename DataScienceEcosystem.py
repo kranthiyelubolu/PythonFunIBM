53cdca4daf01f337fab571991f9552689f897e98
kranthiyelubolu <43675513+kranthiyelubolu@users.noreply.github.com>
2024-10-05 19:44:18 +0530

    Add files via upload

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # (Data Science Tools and Ecosystem)
+
+# #### In this notebook, Data Science Tools and Ecosystem are summarized.
+
+# ##### <b>Some of the popular languages that Data Scientists use are: Python, R,SAS<b>
+
+# #### Some of the commonly used libraries used by Data Scientists include: Pandas, NumPy, Seaborn,Scikit-learn
+
+# | Data Science Tools |
+# |--------------------|
+# | Pandas             |
+# | NumPy              |
+# | Matplotlib         |
+# | Scikit-learn       |
+# | TensorFlow         |
+# | Keras              |
+# | PyTorch            |
+# | Seaborn            |
+# | Plotly             |
+# | RStudio            |
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python
+# 
+# - **Addition**: `3 + 5 = 8`
+# - **Subtraction**: `10 - 4 = 6`
+# - **Multiplication**: `6 * 7 = 42`
+# - **Division**: `20 / 4 = 5`
+
+# In[5]:
+
+
+##This a simple arithmetic expression to mutiply then add integers:
+(8*9)+3
+
+
+# In[6]:
+
+
+## This will convert 300 minutes to hours by diving by 60:
+300/60
+
+
+# - **Components of a Data Scientist's Toolkit**:
+#   - Data collection and storage tools
+# 
+# 
+# - **Tool Categories**:
+#   - Data manipulation and analysis
+# 
+# 
+# - **Examples of Open-Source Tools Used in Data Science**:
+#   - Python
+# 
+# 
+# - **Relevant Features of Open-Source Tools**:
+#   - Free to use and modify
+#   
+# 
+# - **Examples of Commercial and Cloud-Based Data Science Tools**:
+# 
+#  
+
+# ## Author - Kranthi
+
+# In[ ]:
+
+
+
+
